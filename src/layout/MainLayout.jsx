40d47cab3ecfe09c5c09db6c56fa9fe5845273f4
kyachingprue <kyachingpruemarma.info@gmail.com>
2026-02-07 18:16:14 +0600

    new code updated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 
 const MainLayout = () => {
   return (
-    <div className='max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl 2xl:max-w-8xl mx-auto bg-[#0d0c1d]'>
+    <div className='w-full px-2 md:px-4 lg:px-5 mx-auto bg-[#110b5f]'>
       <Navbar />
       <Outlet />
       <Footer/>
