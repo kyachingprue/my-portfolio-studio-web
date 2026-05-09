add2a0f578de2360b9441a15e1d8bb144527a00b
kyachingprue <kyachingpruemarma.info@gmail.com>
2026-05-09 22:57:22 +0600

    new code updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,17 +22,18 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full top-0 left-0 z-50 transition-all duration-300 ${isScrolled ? "fixed bg-black/50 shadow-lg" : "absolute bg-black/50"
-        }`}
+      className={`w-full top-0 left-0 z-50 transition-all duration-300 ${
+        isScrolled ? 'fixed bg-black/50 shadow-lg' : 'absolute bg-black/50'
+      }`}
     >
       <div className="max-w-7xl mx-auto flex justify-between items-center p-4 text-white">
         {/* Left Side */}
         <div className="text-xl md:text-3xl font-bold">
           <Link
             to="/"
-            className="bg-clip-text text-transparent bg-linear-to-r from-indigo-400 via-cyan-400 to-purple-400 animate-gradient-x"
+            className="bg-clip-text flex items-center text-transparent bg-linear-to-r from-indigo-400 via-cyan-400 to-purple-400 animate-gradient-x"
           >
-            Portfolio Web
+            Portfolio <span className="hidden md:block">Web</span>
           </Link>
         </div>
 
@@ -42,7 +43,7 @@ const Navbar = () => {
             to="/"
             className={({ isActive }) =>
               `relative text-sm md:text-base transition-colors duration-300
-     ${isActive ? "text-indigo-300 font-semibold" : "text-white hover:text-gray-400"}
+     ${isActive ? 'text-indigo-300 font-semibold' : 'text-white hover:text-gray-400'}
      after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0
      after:bg-indigo-500 after:transition-all after:duration-300
      hover:after:w-full`
@@ -51,10 +52,11 @@ const Navbar = () => {
             Home
           </NavLink>
 
-          <NavLink to="/my-projects"
+          <NavLink
+            to="/my-projects"
             className={({ isActive }) =>
               `relative text-sm md:text-base transition-colors duration-300
-     ${isActive ? "text-indigo-300 font-semibold" : "text-white hover:text-gray-400"}
+     ${isActive ? 'text-indigo-300 font-semibold' : 'text-white hover:text-gray-400'}
      after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0
      after:bg-indigo-500 after:transition-all after:duration-300
      hover:after:w-full`
@@ -66,7 +68,7 @@ const Navbar = () => {
             to="/contact-me"
             className={({ isActive }) =>
               `relative text-sm md:text-base transition-colors duration-300
-     ${isActive ? "text-indigo-300 font-semibold" : "text-white hover:text-gray-400"}
+     ${isActive ? 'text-indigo-300 font-semibold' : 'text-white hover:text-gray-400'}
      after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0
      after:bg-indigo-500 after:transition-all after:duration-300
      hover:after:w-full`
@@ -77,9 +79,9 @@ const Navbar = () => {
           <a
             href="/kyachingprue_cv.pdf"
             download
-            className="hidden sm:inline-flex items-center gap-2 px-3 py-1.5 rounded-xl 
-  bg-linear-to-r from-blue-600 to-indigo-600 
-  text-white font-medium 
+            className="hidden sm:inline-flex items-center gap-2 px-3 py-1.5 rounded-xl
+  bg-linear-to-r from-blue-600 to-indigo-600
+  text-white font-medium
   shadow-lg shadow-blue-500/20
   hover:from-blue-500 hover:to-indigo-500
   hover:shadow-xl hover:shadow-blue-500/30
@@ -93,16 +95,19 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                d="M12 16v-8m0 8l-3-3m3 3l3-3M5 20h14" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 16v-8m0 8l-3-3m3 3l3-3M5 20h14"
+              />
             </svg>
-
             Download CV
           </a>
         </div>
       </div>
     </nav>
-  );
+  )
 };
 
 export default Navbar;
